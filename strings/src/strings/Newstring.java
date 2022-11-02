b79f7f86b6efde0e230829a5d6c1d68ea290882d
shanmukavenkat <21bq1Aa4208@vvit.net>
2022-11-02 22:08:24 +0530

    strings

---
+++ strings/src/strings/Newstring.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class Newstring {
+	public static void main(String[] args) {
+	String s ="i love java";
+	//comments 
+	//this is single line comment
+	/*multi liline comments are been 
+	  written like this
+	 */
+    System.out.print(s);
+    //to print multi line strings
+    String s2 ="1.rollsroyce";
+    System.out.print(s2);
+    
+	}
+}
